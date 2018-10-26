94dc7e8df0cf684ac060631fa46fb4b0844f6eb2
Ilya Dovban <i.dovban@godeltech.com>
2018-10-26 14:36:38 +0300

    CSS transitions

--- index.js
+++ index.js
@@ -6,12 +6,14 @@ const QUESTIONS = [
 ]
 
 function renderAnswer(answer) {
-    return `<p class="quiz-entry__answer">${answer}</p>`;
+    return `<p class="quiz-entry__answer">
+        <span class="quiz-entry__answer-button">${answer}</span>
+    </p>`;
 }
 
 function renderQuestion({ question, answers }) {
     return `
-      <div class="quiz-entry">
+      <div class="quiz-entry quiz-entry_transition_in">
         <p class="quiz-entry__question">${question}</p>
         ${answers.map(renderAnswer).join('')}
       </div>`;
@@ -27,4 +29,11 @@ function createRoot() {
 document.addEventListener('DOMContentLoaded', () => {
     const root = createRoot();
     root.innerHTML = renderQuestion(QUESTIONS[0]);
+    setTimeout(() => {
+        document.querySelector('.quiz-entry').classList.remove('quiz-entry_transition_in');
+    }, 10)
+
+    document.body.onclick =() => {
+        document.querySelector('.quiz-entry').classList.add('quiz-entry_transition_out');
+    }
 });
